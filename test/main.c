0d4c825afe21494c186d13db3c6de9ea70dc50e3
jwonee1343 <jwonee1343@github.com>
2023-01-16 13:31:03 +0900

    Updated test code for stack

--- test/main.c
+++ test/main.c
@@ -1,12 +1,15 @@
 #include <stdio.h>
 #include <stdint.h>
 #include "queue.h"
+#include "stack.h"
 
 typedef QUEUE_STRUCT(int, 10) queue_t; 
+typedef STACK_STRUCT(int, 10) stack_t;
 
 int main(int argc, char *argv[])
 {
 	queue_t q;
+	stack_t s;
 	uint32_t i = 0;
 
 	QUEUE_INIT(&q);
@@ -17,6 +20,16 @@ int main(int argc, char *argv[])
 
 	while (!QUEUE_EMPTY(&q)) {
 		printf("queue_get() -> %d\n", QUEUE_GET(&q));
+	}
+
+	STACK_INIT(&s);
+	for (i = 0; !STACK_FULL(&s); i++) {
+		printf("stack_push() -> %d\n", i);
+		STACK_PUSH(&s, i);
+	}
+
+	while (!STACK_EMPTY(&s)) {
+		printf("stack_pop() -> %d\n", STACK_POP(&s));
 	}
 
 	return 0;
